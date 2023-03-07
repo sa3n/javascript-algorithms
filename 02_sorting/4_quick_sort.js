d1131a898b17ea17f88135bc1cae4af3a2844d73
LvovNikita <lvov.nikita@yahoo.com>
2023-03-07 12:27:51 +0300

    Edit Quicksort

--- 02_sorting/4_quick_sort.js
+++ 02_sorting/4_quick_sort.js
@@ -1,9 +1,9 @@
 function quickSort(arr) {
     if (arr.length <= 1) return arr
     const anchor = arr.pop()
-    const ltAnchor = arr.filter(elem => elem < anchor)
-    const gteAnchor = arr.filter(elem => elem >= anchor)
-    return [...quickSort(ltAnchor), anchor, ...quickSort(gteAnchor)]
+    const ltAnchor = quickSort(arr.filter(elem => elem < anchor))
+    const gteAnchor = quickSort(arr.filter(elem => elem >= anchor))
+    return [...ltAnchor, anchor, ...gteAnchor]
 }
 
 module.exports = quickSort
