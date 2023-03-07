ad8fae07aa5d5fdb01887a385c212f2aaf99400a
LvovNikita <lvov.nikita@yahoo.com>
2023-03-07 13:46:28 +0300

    Fix tree DFS: use generators

---
+++ 04_trees/0_practice.js
@@ -0,0 +1,11 @@
+const tree = require('../00_sample-data/tree')
+
+// IMPLEMENT:
+
+// 1. DFS
+
+function * DFS(root) {}
+
+// 2. BFS
+
+function BFS(root) {}
--- 04_trees/1_dfsTree.js
+++ 04_trees/1_dfsTree.js
@@ -1,12 +1,8 @@
-const tree = require('../00_sample-data/tree')
-
-function dfsTree(treeNode) {
-    console.log(treeNode.value)
-    if (treeNode.children.length) {
-        for (child of treeNode.children) {
-            dfsTree(child)
-        }
+function * DFS(root) {
+    yield root
+    for (const child of root.children) {
+        yield * DFS(child)
     }
 }
 
-dfsTree(tree.root) // 1 2 4 5 6 7 8 3
+module.exports = DFS
